bf24d4f691e41ea83d90f6d334dbf7b4db1bc8fe
ShankulR <10er44shankulr@gmail.com>
2026-01-04 22:43:19 +0530

    Add files via upload

---
+++ engine_room(12).py
@@ -0,0 +1,14 @@
+import math
+
+math.sqrt(4)
+
+from math import sqrt       # use ctrl + tab to get suggestions after import
+sqrt(4) 
+
+math.log10(100)
+
+math.sqrt(60)
+
+math.pi
+
+print(today.day, today.month, today.year)
